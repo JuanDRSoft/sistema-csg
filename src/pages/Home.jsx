5068151aab25cc6bc4379f186c9a63f1fdb13b3e
JuanDRSoft <95110527+JuanDRSoft@users.noreply.github.com>
2023-12-30 14:52:57 -0500

    feat/ filter events

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import useAuth from "../hooks/useAuth";
 import { Link } from "react-router-dom";
 
 const Home = () => {
+  const [tab, setTab] = useState(1);
+
   const { events } = useAuth();
 
   return (
@@ -10,10 +12,20 @@ const Home = () => {
       <h1 className="font-bold text-3xl">Eventos</h1>
 
       <div className="mt-10 flex gap-5">
-        <button className="border-b-2 border-pink-600 p-2 font-semibold">
+        <button
+          onClick={() => setTab(1)}
+          className={`border-b-2 ${
+            tab == 1 && "border-pink-600"
+          } p-2 font-semibold`}
+        >
           Activos
         </button>
-        <button className="border-b-2 border-pink-600 p-2 font-semibold">
+        <button
+          onClick={() => setTab(2)}
+          className={`border-b-2 ${
+            tab == 2 && "border-pink-600"
+          } p-2 font-semibold`}
+        >
           Procesados
         </button>
       </div>
@@ -25,16 +37,33 @@ const Home = () => {
           <p className="w-full text-end">Creado</p>
         </div>
 
-        {events.map((e) => (
-          <Link
-            to={`/app/events/${e.id}`}
-            className="bg-white p-5 mb-3 flex items-center gap-5 rounded-xl shadow"
-          >
-            <p className="w-full">{e.device.name}</p>
-            <p className="w-full text-center">{e.type}</p>
-            <p className="w-full text-end">{e.device.created_at}</p>
-          </Link>
-        ))}
+        {tab == 1 &&
+          events
+            .filter((e) => !e.status || e.status == "Unprocessed")
+            .map((e) => (
+              <Link
+                to={`/app/events/${e.id}`}
+                className="bg-white p-5 mb-3 flex items-center gap-5 rounded-xl shadow"
+              >
+                <p className="w-full">{e.device.name}</p>
+                <p className="w-full text-center">{e.type}</p>
+                <p className="w-full text-end">{e.device.created_at}</p>
+              </Link>
+            ))}
+
+        {tab == 2 &&
+          events
+            .filter((e) => e.status == "Processed")
+            .map((e) => (
+              <Link
+                to={`/app/events/${e.id}`}
+                className="bg-white p-5 mb-3 flex items-center gap-5 rounded-xl shadow"
+              >
+                <p className="w-full">{e.device.name}</p>
+                <p className="w-full text-center">{e.type}</p>
+                <p className="w-full text-end">{e.device.created_at}</p>
+              </Link>
+            ))}
       </div>
     </div>
   );
